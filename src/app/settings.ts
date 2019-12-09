fcc4538d6250c86e64d0df4703307f4adb19a719
crenouleau <ced2411@gmail.com>
2019-12-09 22:53:58 +0100

    sans http

--- src/app/settings.ts
+++ src/app/settings.ts
@@ -1,4 +1,4 @@
 export const settings = {
   // apiUrl: 'https://romantic-sammet-433123.netlify.com/api/v1',
-  apiUrl: 'http://myappserverrpi.ddns.net/api/v1',
+  apiUrl: '://myappserverrpi.ddns.net/api/v1',
 }
